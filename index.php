542d571e3b9d4adddb921b40311a2fd9fb5fdcaa
nilesh <nilesh@tradesocio.com>
2018-10-01 16:53:00 +0530

    sss

--- index.php
+++ index.php
@@ -9,27 +9,34 @@ if($method == 'POST'){
 
 	$text = $json->queryResult->intent->displayName;
 
-	echo $text;exit;
+	echo "print";	
+	echo $json;
 
 	switch ($text) {
 		case 'hi':
+			echo "inside hi";
 			$json->queryResult->fulfillmentMessages->text->text = "Hi, Nice to meet you";
 			break;
 
 		case 'bye':
+			echo "inside bye";
 			$json->queryResult->fulfillmentMessages->text->text = "Bye, good night";
 			break;
 
 		case 'anything':
+			echo "inside anything";
 			$json->queryResult->fulfillmentMessages->text->text = "Yes, you can type anything here.";
 			break;
 		
 		default:
+			echo "inside default";
 			$json->queryResult->fulfillmentMessages->text->text = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
  
+	echo "inside final call";	
 	echo json_encode($json);
+	exit;
 }
 else
 {
